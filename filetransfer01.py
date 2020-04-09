ef4a645efc7d1ee11334d69881cd1ccd09739287
wgathright <will.gathright@yahoo.com>
2020-04-09 12:04:30 -0500

    Added MD5 hashing

--- filetransfer01.py
+++ filetransfer01.py
@@ -2,6 +2,7 @@ import argparse
 import subprocess
 import os
 import threading
+import hashlib
 
 
 # Array of File objects
@@ -13,10 +14,21 @@ class File(object):
     def __init__(self,fileName):
         self.fileName = fileName # file path
         self.si = ""             # storage island to copy to
+        self.originalMD5 = ""
+        self.copyMD5 = ""
         
         
     def moveFile(self):
         subprocess.call(['rsync','-r', self.fileName, self.si])
+        
+        
+    def generateHash(self,fileToCheck):
+        with open(fileToCheck,'rb') as openedFile:
+            fileContents = openedFile.read()
+            md5Returned = hashlib.md5(fileContents).hexdigest()
+            return md5Returned
+            
+        
         
 
 # Build array of file objects from source 
@@ -35,6 +47,7 @@ if __name__ == '__main__':
     # Add parameters positional/optional(--)
     parser.add_argument('source', help="Source")
     #parser.add_argument('destination', help="Destination")
+    #TODO: add rsync options (-avrz) -v verbose, -a archive, -z compress
     
     
     
@@ -45,15 +58,14 @@ if __name__ == '__main__':
     
     buildList(mySource)
         
-     
+    # Build array of Storage Islands 
     for entry in os.scandir('/home/pi/TestStorage'):
         islands.append(entry.path)
         
     print("Number of storage islands available: ",len(islands))
     
     currentIsland = 0   
-    # TODO: for storageIslands in /TestStorage assign a si to each file in files array
-    # if ('df -h' < 75%) ->  assign; else pass
+    #TODO: if ('df -h' < 75%) ->  assign; else pass
     for file in files:
         
         if currentIsland < len(islands):
@@ -66,8 +78,7 @@ if __name__ == '__main__':
     
     
     threads = []
-
-    # create subprocess to run rsync
+    # create threaded moveFile() 
     for file in files:
         t = threading.Thread(target=file.moveFile)
         t.start()
@@ -79,7 +90,11 @@ if __name__ == '__main__':
     
     
    
-    # TODO: Implement checksum after rsync complete -- hashlib?   
+    # TODO: Implement checksum after rsync complete -- hashlib?
+    for file in files:
+        file.originalMD5 = file.generateHash(file.fileName)
+        print(file.originalMD5)
+        
     
     
     
